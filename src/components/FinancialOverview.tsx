8db0b30c9e0cdfeda6dee1d2017f5628e300086e
yonatan <yonatanasmare31@gmail.com>
2025-09-19 09:25:10 +0300

    changed the back-end package.json

--- src/components/FinancialOverview.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FinancialOverview = () => {
-  return (
-    <div className='flex-1 rounded-lg shadow bg-white'>Financial Overview</div>
-  )
-}
-
-export default FinancialOverview
--- src/components/Last30DaysExpense.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Last30DaysExpense = () => {
-    return (
-    <div className='flex-1 rounded-lg shadow bg-white'>Last 30 Days Expenses</div>
-    )
-}
-
-export default Last30DaysExpense
